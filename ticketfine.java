7fd996c565b596ae245e6ea74411b1a63fcb47b7
Priyanshu <priyanshudashofficial@gmail.com>
2022-12-05 23:05:18 +0530

    day11

---
+++ ticketfine.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class ticketfine
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-- > 0)
+		{
+		   int x=sc.nextInt();
+		   int p=sc.nextInt();
+		   int q=sc.nextInt();
+		   int r=x*(p-q);
+		   System.out.println(r);
+		}
+	}
+}
+
