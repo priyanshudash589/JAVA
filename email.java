e2161d6607bde583237346eb5b68820881af80a6
Priyanshu <priyanshudashofficial@gmail.com>
2022-12-07 01:38:55 +0530

    day13

---
+++ email.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class email
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int n=sc.nextInt();
+		int u=sc.nextInt();
+		System.out.println(n-u);
+	}
+}
