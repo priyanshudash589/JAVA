2c7caa1d18b36f38f309bf3b116cf9c48b812929
Priyanshu Dash <pdash3774@gmail.com>
2024-04-19 18:21:05 +0530

    pattern updated

---
+++ Pattern/pattern5.java
@@ -0,0 +1,20 @@
+package JAVA.Pattern;
+
+public class pattern5 {
+    public static void main(String[] args){
+        /*
+            *****
+            ****
+            ***
+            **
+            *
+         */
+
+         for(int i = 1 ; i <= 5 ; i++){
+            for(int j= i; j <=5; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+         }
+    }
+}
